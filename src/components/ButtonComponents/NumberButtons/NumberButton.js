13b9d4b22ad38fc7f2e93358f4d30e69853edd06
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-08 13:50:46 -0700

    changed numberbutton div to button

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -6,13 +6,13 @@ import React from "react";
 const NumberButton = (props) => {
 
   return (
-    <div className="numbers-button">
+    <button className="numbers-button">
 
       {/* Display a button element rendering the data being passed down from the parent container on props */
         props.numberVal}
 
 
-    </div>
+    </button>
   );
 };
 
