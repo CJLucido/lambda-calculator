c1b987e8a179aa1e7bf32215afc1379eda11d1f9
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-08 13:49:44 -0700

    changed operator button div to button and the property being called to char instead of it's actual JS value

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const OperatorButton = (props) => {
   return (
-    <div>
-      {/* Display a button element rendering the data being passed down from the parent container on props */props.seen.value}
-    </div>
+    <button>
+      {/* Display a button element rendering the data being passed down from the parent container on props */props.seen.char}
+    </button>
   );
 };
 
