262d05a2c4615149075039479feae46987724270
CJ Lucido <c.jose.lucido@gmail.com>
2019-10-08 13:18:27 -0700

    changed node modules and package-lock so that I could npm start to localhost 3000, added state and imported info from data to Numbers.js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import {numbers} from "../../../data";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -8,12 +9,19 @@ import { numbers } from '../../../data'
 //Import your array data to from the provided data file
 
 const Numbers = () => {
+
+    const [numberFor] = useState(numbers)
+
   // STEP 2 - add the imported data to state
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+      numberFor.map((button, index) => (
+        <NumberButton key={index} numberVal={button}/>
+      ))
+      }
     </div>
   );
 };
